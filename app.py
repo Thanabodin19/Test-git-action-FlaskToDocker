b1434e1b0b35e0e1fcdf878b1855c6aaedd71484
Thanabodin19 <thanabodin.kea@gmail.com>
2024-09-21 15:32:36 +0700

    return "CI/CD test"

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Sameera, I'm Flask"
+    return "CI/CD test"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5002)
